a3d2f529bd2abda3ba2ca03a85b61f8d7fe25b9c
Andreas Drobisch <dro@unkonstant.de>
2015-01-18 23:57:33 +0100

    show referenced item in timeline

--- putput-data-server/web/src/js/item/ItemModule.js
+++ putput-data-server/web/src/js/item/ItemModule.js
@@ -1,36 +1,22 @@
 'use strict';
 
 var angular = require('angular');
-var ItemController = require("./ItemController");
 
 var itemModule = angular.module('item', []);
 
-itemModule.controller("ItemController", ItemController);
-
 itemModule.config(['$routeProvider', function($routeProvider) {
-
   $routeProvider.when('/item/:itemId', {
-    controller: ItemController,
-    template: require('./item.html')
+    controller: require("./ItemViewController"),
+    template: require('./item-view.html')
   });
-
-}]);
-
-itemModule.directive("streamItem", function () {
-    return {
-        controller: ItemController,
-        scope: {
-            "item" : "=item"
-        },
-        template: require('./itemTemplate.html')
-    };
-});
-
-itemModule.run(['$templateCache', function (templateCache) {
-    templateCache.put("user-mentions.tpl", require("./user-mentions.html"));
-    templateCache.put("emoji.tpl", require("./emoji.html"));
 }]);
 
 itemModule.directive("itemInput", require("./ItemInputDirective"));
+itemModule.directive("streamItem", require("./ItemDirective"));
+
+itemModule.run(['$templateCache', function (templateCache) {
+    templateCache.put("user-mentions.tpl", require("./user-mentions.html"));
+    templateCache.put("emoji.tpl", require("./emoji-list.html"));
+}]);
 
 module.exports = itemModule;
