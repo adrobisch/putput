d0c61377b72882564c89a080c208302cb28cfdd6
Andreas Drobisch <dro@unkonstant.de>
2015-03-03 19:59:33 +0100

    feat(contacts): confirm deletion of contact

--- putput-data-server/web/src/js/app.js
+++ putput-data-server/web/src/js/app.js
@@ -50,4 +50,22 @@ app.config(['$routeProvider', function($routeProvider) {
   });
 }]);
 
+app.directive('ngConfirmClick', [
+  function(){
+    return {
+      priority: -1,
+      restrict: 'A',
+      link: function(scope, element, attrs){
+        element.bind('click', function(e){
+          var message = attrs.ngConfirmClick;
+          if(message && !confirm(message)){
+            e.stopImmediatePropagation();
+            e.preventDefault();
+          }
+        });
+      }
+    }
+  }
+]);
+
 module.exports = app;
