9b68353ae2ed088abedc4d5d7d39390c213d8de0
adrobisch <andreas@drobisch.com>
2016-06-20 00:19:06 +0200

    feat(web): forward http to https for X-Forwarded-Proto

--- putput-data-server/src/main/java/org/putput/common/web/config/WebConfig.java
+++ putput-data-server/src/main/java/org/putput/common/web/config/WebConfig.java
@@ -2,13 +2,22 @@ package org.putput.common.web.config;
 
 import com.github.ziplet.filter.compression.CompressingFilter;
 import org.putput.common.web.filter.AllowAllCorsFilter;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.context.embedded.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
+import org.springframework.web.filter.OncePerRequestFilter;
 import org.springframework.web.servlet.config.annotation.PathMatchConfigurer;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 
 @Configuration
 public class WebConfig extends WebMvcConfigurerAdapter {
@@ -19,6 +28,9 @@ public class WebConfig extends WebMvcConfigurerAdapter {
   private static final String[] CLASSPATH_RESOURCE_LOCATIONS = {
     "classpath:/META-INF/resources/", "classpath:/resources/",
     "classpath:/static/", "classpath:/public/"};
+
+  @Autowired
+  Environment environment;
 
   @Override
   public void addResourceHandlers(ResourceHandlerRegistry registry) {
@@ -56,6 +68,24 @@ public class WebConfig extends WebMvcConfigurerAdapter {
   @Bean
   public AllowAllCorsFilter corsFilter() {
     return new AllowAllCorsFilter();
+  }
+
+  @Bean
+  public OncePerRequestFilter protocolForwardFilter() {
+    return new OncePerRequestFilter() {
+      @Override
+      protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        String forwardedProtocolHeader = request.getHeader("X-Forwarded-Proto");
+        boolean forwardToHttps = Boolean.parseBoolean(environment.getProperty("forward.to.https", "true"));
+
+        if (forwardToHttps && forwardedProtocolHeader != null && forwardedProtocolHeader.equals("http")) {
+          response.sendRedirect(environment.getProperty("secure.base.url", "https://putput.org") + request.getRequestURI());
+        } else {
+          filterChain.doFilter(request, response);
+        }
+
+      }
+    };
   }
 
   @Override
